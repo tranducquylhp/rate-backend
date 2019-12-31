1cc37528ccb15684d1db817d65a08c8556505baa
Tran Duc Quy <tranducquylhp@gmail.com>
2019-12-31 19:02:32 +0700

    add model StandardOutput

--- src/main/java/com/codegym/rate/model/StandardOutput.java
+++ src/main/java/com/codegym/rate/model/StandardOutput.java
@@ -9,4 +9,50 @@ public class StandardOutput {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
+    private String rate;
+
+    @ManyToOne
+    @JoinColumn(name = "studyProgram_id")
+    private StudyProgram studyProgram;
+
+    public StandardOutput() {
+    }
+
+    public StandardOutput(String name, String rate, StudyProgram studyProgram) {
+        this.name = name;
+        this.rate = rate;
+        this.studyProgram = studyProgram;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public StudyProgram getStudyProgram() {
+        return studyProgram;
+    }
+
+    public void setStudyProgram(StudyProgram studyProgram) {
+        this.studyProgram = studyProgram;
+    }
+
+    public String getRate() {
+        return rate;
+    }
+
+    public void setRate(String rate) {
+        this.rate = rate;
+    }
 }
