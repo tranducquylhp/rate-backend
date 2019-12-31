72d932b16860005e167541bdb5c19477f8cbc65a
Tran Duc Quy <tranducquylhp@gmail.com>
2019-12-31 19:26:27 +0700

    add verificationToken repository

---
+++ src/main/java/com/codegym/rate/repository/VerificationTokenRepository.java
@@ -0,0 +1,10 @@
+package com.codegym.rate.repository;
+
+import com.codegym.rate.model.VerificationToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
+    VerificationToken findByToken(String token);
+}
