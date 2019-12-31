42f0e892826e7987d275ee4acc3c43659e3fb815
Tran Duc Quy <tranducquylhp@gmail.com>
2019-12-31 18:58:44 +0700

    add model StudyProgram

--- src/main/java/com/codegym/rate/model/StandardOutput.java
+++ src/main/java/com/codegym/rate/model/StandardOutput.java
@@ -1,4 +1,12 @@
 package com.codegym.rate.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "standardOutput")
 public class StandardOutput {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
 }
--- src/main/java/com/codegym/rate/model/StudyProgram.java
+++ src/main/java/com/codegym/rate/model/StudyProgram.java
@@ -1,4 +1,60 @@
 package com.codegym.rate.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "studyProgram")
 public class StudyProgram {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "instructor_id")
+    private Instructor instructor;
+
+    @OneToMany(targetEntity = StandardOutput.class)
+    private StandardOutput standardOutput;
+
+    public StudyProgram() {
+    }
+
+    public StudyProgram(String name, Instructor instructor, StandardOutput standardOutput) {
+        this.name = name;
+        this.instructor = instructor;
+        this.standardOutput = standardOutput;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+    public StandardOutput getStandardOutput() {
+        return standardOutput;
+    }
+
+    public void setStandardOutput(StandardOutput standardOutput) {
+        this.standardOutput = standardOutput;
+    }
 }
