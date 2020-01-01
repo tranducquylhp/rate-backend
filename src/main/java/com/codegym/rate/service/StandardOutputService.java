b1c94a064f0aeb76a57aaa4f01565777f9c78dc7
Tran Duc Quy <tranducquylhp@gmail.com>
2020-01-01 17:23:23 +0700

    add standard output service

---
+++ src/main/java/com/codegym/rate/service/StandardOutputService.java
@@ -0,0 +1,6 @@
+package com.codegym.rate.service;
+
+import com.codegym.rate.model.StandardOutput;
+
+public interface StandardOutputService extends GeneralService<StandardOutput> {
+}
---
+++ src/main/java/com/codegym/rate/service/impl/StandardOutputServiceImpl.java
@@ -0,0 +1,34 @@
+package com.codegym.rate.service.impl;
+
+import com.codegym.rate.model.StandardOutput;
+import com.codegym.rate.repository.StandardOutputRepository;
+import com.codegym.rate.service.StandardOutputService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class StandardOutputServiceImpl implements StandardOutputService {
+    @Autowired
+    private StandardOutputRepository standardOutputRepository;
+    @Override
+    public List<StandardOutput> findAll() {
+        return standardOutputRepository.findAll();
+    }
+
+    @Override
+    public void save(StandardOutput standardOutput) {
+        standardOutputRepository.save(standardOutput);
+    }
+
+    @Override
+    public StandardOutput findById(Long id) {
+        Optional<StandardOutput> standardOutput = standardOutputRepository.findById(id);
+        if (standardOutput.isPresent()){
+            return standardOutput.get();
+        }
+        return null;
+    }
+}
