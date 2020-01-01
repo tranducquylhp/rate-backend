0818693fdb206a7914c23bde9f4aa529b39835b5
Tran Duc Quy <tranducquylhp@gmail.com>
2020-01-01 16:44:08 +0700

    add login, register, new password, get user current

---
+++ src/main/java/com/codegym/rate/controller/UserRestController.java
@@ -0,0 +1,128 @@
+package com.codegym.rate.controller;
+
+import com.codegym.rate.model.JwtResponse;
+import com.codegym.rate.model.Role;
+import com.codegym.rate.model.User;
+import com.codegym.rate.model.VerificationToken;
+import com.codegym.rate.service.RoleService;
+import com.codegym.rate.service.UserService;
+import com.codegym.rate.service.VerificationTokenService;
+import com.codegym.rate.service.impl.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Optional;
+
+@RestController
+public class UserRestController {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+
+    @Autowired
+    private VerificationTokenService verificationTokenService;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private RoleService roleService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @GetMapping("/users/{userId}")
+    public ResponseEntity<User> findById(@PathVariable Long userId){
+        User user = userService.findById(userId);
+        if (user == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<User> findUserByUsername(@RequestParam("username") String username){
+        User user = userService.findByUsername(username);
+        if (user == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user, BindingResult bindingResult) {
+        if (bindingResult.hasFieldErrors()) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        if (!userService.isCorrectConfirmPassword(user)) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        Role role = user.getRoles();
+        if (roleService.findRoleByName(role.getName()) == null){
+            roleService.save(role);
+        } else role = roleService.findRoleByName(role.getName());
+        user.setRoles(role);
+        Iterable<User> users = userService.findAll();
+        for (User currentUser : users) {
+            if (currentUser.getUsername().equals(user.getUsername())) {
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+        }
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setConfirmPassword(passwordEncoder.encode(user.getConfirmPassword()));
+        userService.save(user);
+        VerificationToken token = new VerificationToken(user);
+        token.setExpiryDate(10);
+        verificationTokenService.save(token);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody User user) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = jwtService.generateTokenLogin(authentication);
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+
+        return ResponseEntity.ok(new JwtResponse(jwt, userDetails.getUsername(), userDetails.getAuthorities()));
+    }
+
+    @RequestMapping(value = "/new-password/{id}", method = {RequestMethod.POST})
+    public ResponseEntity<User> updatePassword(@PathVariable Long id, @RequestBody User user) {
+        Optional<User> userOptional = Optional.ofNullable(userService.findById(id));
+        if (!userOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        if (!userService.isCorrectConfirmPassword(user)) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        String newPassword = passwordEncoder.encode(user.getPassword());
+        String confirmPassword = passwordEncoder.encode(user.getConfirmPassword());
+        user.setPassword(newPassword);
+        user.setConfirmPassword(confirmPassword);
+        userService.save(user);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @GetMapping("/userCurrent")
+    public ResponseEntity<User> userCurrent(){
+        User user = userService.getCurrentUser();
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+}
