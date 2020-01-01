05e3e443df60eab6c5459faeeb958c37a358e2ad
Tran Duc Quy <tranducquylhp@gmail.com>
2020-01-01 17:20:44 +0700

    add standard output repository

---
+++ src/main/java/com/codegym/rate/repository/StandardOutputRepository.java
@@ -0,0 +1,7 @@
+package com.codegym.rate.repository;
+
+import com.codegym.rate.model.StandardOutput;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StandardOutputRepository extends JpaRepository<StandardOutput, Long> {
+}
