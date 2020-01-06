7e3e0275f62692e487cd23673c79d500ef656a7a
Tran Duc Quy <tranducquylhp@gmail.com>
2020-01-06 09:34:16 +0700

    repository tìm kiếm user theo tên

--- src/main/java/com/codegym/rate/repository/UserRepository.java
+++ src/main/java/com/codegym/rate/repository/UserRepository.java
@@ -3,7 +3,10 @@ import com.codegym.rate.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
+    List<User> findAllByNameContaining(String name);
 }
